b1a1157f2e02ef12d54d1809a501547bfcccd1e8
Ian Percy <ianpercyor@gmail.com>
2019-12-18 10:54:01 -0800

    adjust resp message first

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({hi: 'there'});
+  res.send({hi: 'I love you lola});
 });
 
 const PORT = process.env.PORT || 5000;
