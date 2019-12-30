35c0027b0700cf110bb9f2c772a604c42fa96704
Ian Percy <ianpercyor@gmail.com>
2019-12-30 14:30:42 -0800

    pushing verification

---
+++ routes/authRoutes.js
@@ -0,0 +1,32 @@
+const passport = require('passport');
+
+module.exports = (app) =>{
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  );
+
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google')
+  );
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user); // user will be logged out and get back result
+  });
+
+  app.get(
+    '/api/current_user',
+    (req, res) => {
+      res.send(req.user);
+    }
+  );
+  app.get('/google181d5db1eb9426de.html',
+  (req, res) => {
+    res.sendFile('../google181d5db1eb9426de.html');
+  });
+
+
+};
