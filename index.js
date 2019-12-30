4a9f3cf31bfe8083bc7768d3e67bf7841a61529b
Ian Percy <ianpercyor@gmail.com>
2019-12-30 15:31:15 -0800

    umm

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const passport = require('passport');
 require('./models/User');
 //require('./services/passport');
 
+const path = require('path');
+const dirPath = path.join(__dirname, '/google181d5db1eb9426de.html');
 
 
 //mongoose.connect(keys.mongoURI);
@@ -21,7 +23,11 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 */
-require('./routes/authRoutes')(app);
+//require('./routes/authRoutes')(app);
 
+app.get('/google181d5db1eb9426de.html',
+(req, res) => {
+  res.sendFile(dirPath);
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
